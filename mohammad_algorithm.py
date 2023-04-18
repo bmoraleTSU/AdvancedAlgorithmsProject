b390c6e7393b16603c86b4c1b01576b03f751ea7
Bryce Morales <100156666+bmoraleTSU@users.noreply.github.com>
2023-04-18 17:10:54 -0500

    Updated mohammad_algorithm.py
    Added arguments to sample function and comments explaining them.

--- mohammad_algorithm.py
+++ mohammad_algorithm.py
@@ -1,4 +1,6 @@
 #This is where you will type your code for you algorithm
 
-def xx_algorithm():
+def xx_algorithm(text, pattern):
+    #Use the text argument as the text to search through
+    #Use the pattern argument as the string to look for in the given text
     print("The xx_algorithm() is importing correctly!")
