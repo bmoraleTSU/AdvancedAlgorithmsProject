ed0d7e468f6e0ba62f5630bedaf6994fea4c4c29
Aibjewel <116687546+Aibjewel@users.noreply.github.com>
2023-04-23 11:40:52 -0500

    Update mohammad_algorithm.py

--- mohammad_algorithm.py
+++ mohammad_algorithm.py
@@ -1,16 +1,30 @@
 #This is where you will type your code for you algorithm
+# define a function to perform a naive search for a pattern in a text file
+def naive_search(pattern, text):
+    
+   # Initialize a counter to count pattern occurrences
+occurrences = 0
 
-def naive_algorithm(text, pattern):
-    start_naive = time.time()
-    n = len(text)
-    m = len(pattern)
+# Iterate over every possible substring in the text with the same length as the pattern
+for i in range(len(text)-len(pattern)+1):
+    
+    # If the current substring matches the pattern, increase the counter
+    if text[i:i+len(pattern)] == pattern:
+        occurrences += 1
 
-    count_naive = sum(
-        [1 for i in range(n - m + 1) if text[i:i + m] == pattern]
-    )
+# Return the number of occurrences
+return occurrences
 
-    return count_naive, time.time() - start_naive
-text = "your_text_here"
-pattern = "your_pattern_here"
-count, time_taken = naive_algorithm(text, pattern)
-print(f"Pattern found {count} times in {time_taken} seconds.")
+# open the input file and read its contents
+with open("input.txt", "r") as file:
+    text = file.read()
+
+# define the pattern to search for
+pattern = "ababada"
+
+# call the naive_search function to search for the pattern in the text file
+count, duration = naive_search(pattern, text)
+
+# print the results of the search
+print(f"Number of occurrences of '{pattern}': {count}")
+print(f"Duration: {duration:.3f} ms")
