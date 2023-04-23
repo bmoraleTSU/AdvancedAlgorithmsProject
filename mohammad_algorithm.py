6c76709185ec5b0c24202efafba574f66e2ec4e6
Aibjewel <116687546+Aibjewel@users.noreply.github.com>
2023-04-23 12:03:51 -0500

    Update mohammad_algorithm.py

--- mohammad_algorithm.py
+++ mohammad_algorithm.py
@@ -2,15 +2,16 @@
 # define a function to perform a naive search for a pattern in a text file
 def naive_search(pattern, text):
     
-   # Initialize a counter to count pattern occurrences
-occurrences = 0
+    # Initialize a counter to count pattern occurrences
+    occurrences = 0
 
-# Iterate over every possible substring in the text with the same length as the pattern
-for i in range(len(text)-len(pattern)+1):
-    
-    # If the current substring matches the pattern, increase the counter
-    if text[i:i+len(pattern)] == pattern:
-        occurrences += 1
+    # Iterate over every possible substring in the text with the same length as the pattern
+    for i in range(len(text)-len(pattern)+1):
+        
+        # If the current substring matches the pattern, increase the counter
+        if text[i:i+len(pattern)] == pattern:
+            occurrences += 1
 
-# Return the number of occurrences
-return occurrences
+    # Return the number of occurrences
+    return occurrences
+
