942b8bd3b6d9101e9aaf8a25b93d3f4a85bd004b
Aibjewel <116687546+Aibjewel@users.noreply.github.com>
2023-04-23 09:17:19 -0500

    Update mohammad_algorithm.py

--- mohammad_algorithm.py
+++ mohammad_algorithm.py
@@ -1,6 +1,16 @@
 #This is where you will type your code for you algorithm
 
-def xx_algorithm(text, pattern):
-    #Use the text argument as the text to search through
-    #Use the pattern argument as the string to look for in the given text
-    print("The xx_algorithm() is importing correctly!")
+def naive_algorithm(text, pattern):
+    start_naive = time.time()
+    n = len(text)
+    m = len(pattern)
+
+    count_naive = sum(
+        [1 for i in range(n - m + 1) if text[i:i + m] == pattern]
+    )
+
+    return count_naive, time.time() - start_naive
+text = "your_text_here"
+pattern = "your_pattern_here"
+count, time_taken = naive_algorithm(text, pattern)
+print(f"Pattern found {count} times in {time_taken} seconds.")
