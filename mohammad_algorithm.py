63a70eeb81ace2124663f6e94612e69ff584aabb
Aibjewel <116687546+Aibjewel@users.noreply.github.com>
2023-05-05 15:16:00 -0500

    Update mohammad_algorithm.py

--- mohammad_algorithm.py
+++ mohammad_algorithm.py
@@ -1,11 +1,21 @@
 #This is where you will type your code for you algorithm
 # define a function to perform a naive search for a pattern in a text file
-def naive_search_algorithm(text, pattern):
-    count = 0
-    for i in range(len(text) - len(pattern) + 1):
-        if text[i:i+len(pattern)] == pattern:
-            count += 1
-    #print(f"Naive search took {(end_time-start_time)*1000:.4f} milliseconds")
+def naive_search(pattern, txt):
+    m = len(pattern)
+    n = len(txt)
+    count =0
+    start_time = time.time()
+    for i in range(n - m + 1):
+        j = 0
+        while(j < m):
+            if (txt[i + j] != pattern[j]):
+                break
+            j += 1
+ 
+        if (j == m):
+          count += 1
+  end_time = time.time()
+    print(f"Naive search took {(end_time-start_time)*1000:.4f} milliseconds")
     return count
 
 
